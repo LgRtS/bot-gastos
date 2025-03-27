56d2842ad9ae1ab5be2b120931442f05334c7653
Gabriel <gabcostaoliv@usp.br>
2025-03-27 08:59:29 -0300

    corrigindo a porta do render

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -38,6 +39,5 @@ def whatsapp_bot():
     return str(response)
 
 if __name__ == "__main__":
-    import os
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
